fbb99ee29466bde3951e0e7d8f20f30b6bb99ae9
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2020-12-05 16:48:09 +0900

    mtgi add

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var count = 0;
-var random_count = Math.round(Math.random() * 6) + 4; //* 幅 )+ 最小
-// slideshow_timer();
+var random_count = 2;
 var sound = new Audio("./curcuit.mp3");
 var call_tm = 0;
 var calling = true;
-// var cont = new Array("call-cont0", "call-cont1", "call-cont2", "call-cont3");
 var cont_c = 0;
 var record = new Array(0);
+var img_c = 0;
+var mtgi_Imgs = new Array(
+  "./mtgi/mtgi-a1.png",
+  "./mtgi/mtgi-2.jpg",
+  "./mtgi/mtgi-a2.png",
+  "./mtgi/mtgi-3.png",
+  "./mtgi/mtgi-a3.png"
+);
 
 $(function () {
   $("a").click(function () {
@@ -14,20 +20,23 @@ $(function () {
     return false;
   });
   console.log(random_count);
-  setSwipe("#cont3");
+  setSwipe("#cont5");
 }); //ホーム追加でsafari起動させない
 
 function slideshow_timer() {
+  if (cont_c == 1) {
+    $(".start-btn").addClass("none");
+    $(".main-cont").removeClass("none");
+  }
   count++;
-  // console.log(count);
-  if (count > 2) {
+  if (count > random_count) {
     sound.repeat = true;
     sound.addEventListener("ended", function () {
       if (!!this.repeat) {
         this.play();
       }
     });
-    $(".call-cont" + cont_c).css({ visibility: "visible" });
+    $("#cont" + cont_c).removeClass("none");
     sound.play();
     timeCount();
     return;
@@ -38,16 +47,24 @@ function slideshow_timer() {
 function timeCount() {
   if (calling) {
     call_tm += 1;
-    // console.log(call_tm);
     setTimeout("timeCount()", 10);
   }
 }
 
 function stopSound(n) {
+  console.log(cont_c);
   sound.repeat = false;
   sound.pause();
+  if (calling) {
+    record.push(call_tm / 100);
+  }
   calling = false;
-  $("#cont" + cont_c).removeClass("call-cont" + cont_c);
+  if (cont_c <= 4) {
+    $("#cont" + cont_c).removeClass("call-cont-button");
+  } else if (cont_c <= 5) {
+    $("#cont" + cont_c).removeClass("call-cont-sw");
+  }
+
   $("#cont" + cont_c).addClass("call-cont1");
   if (n == "0") {
     document.getElementById("call_name" + cont_c).innerHTML = "拒否";
@@ -58,24 +75,24 @@ function stopSound(n) {
   }
 
   document.getElementById("call_time" + cont_c).innerHTML = call_tm / 100;
-  $(".reset").css({ visibility: "visible" });
-  record.push(call_tm / 100);
+  $(".reset").removeClass("none");
 }
 
 function reset() {
-  if (cont_c == 0) {
-    $(".call-cont0").css({ visibility: "hidden" });
-  }
-  $(".call-cont1").css({ visibility: "hidden" });
-  $(".reset").css({ visibility: "hidden" });
+  $("#cont" + cont_c).addClass("none");
+  $(".reset").addClass("none");
   count = 0;
-  random_count = Math.round(Math.random() * 6) + 4;
+  random_count = Math.round(Math.random() * 20) + 15; //* 幅 )+ 最小
   console.log(random_count);
   call_tm = 0;
   calling = true;
   sound.currentTime = 0;
-  if (cont_c < 3) {
+  if (cont_c < 5) {
+    //デザイン個数入力
     cont_c += 1;
+    if (cont_c != 1) {
+      slideshow_timer();
+    }
   } else {
     record.shift(); //先頭（test）を削除
     document.getElementById("reco").innerHTML = record;
@@ -114,5 +131,18 @@ function setSwipe(elem) {
       // 上から下にスワイプ
       stopSound(1);
     }
+    if (startX > moveX && startX > moveX + dist) {
+      // 右から左にスワイプ
+    } else if (startX < moveX && startX + dist < moveX) {
+      // 左から右にスワイプ
+    }
   });
+}
+
+function nextImg() {
+  document.getElementById("mtgi").src = mtgi_Imgs[img_c];
+  img_c += 1;
+  if (img_c == 2) {
+    document.getElementById("mtgi-disc").innerHTML = "間違いは全部で7箇所";
+  }
 }
